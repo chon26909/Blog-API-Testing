b9ba0e79f18a51c86a844ed66f488bb8e1b7016c
chon26909 <59905927+chon26909@users.noreply.github.com>
2021-11-18 22:34:55 +0700

    update post route

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -2,7 +2,11 @@ const express = require("express")
 const router = express.Router();
 const postController = require("../controllers/postController")
 
+
+router.get("/search",postController.searchPostByText)
 router.get("/",postController.getAllPost);
+router.get("/:id",postController.getPostById);
 router.post("/",postController.createPost);
+router.delete("/:id",postController.deletePost)
 
 module.exports = router;
